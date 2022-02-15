3f89319168d92b8349e30d874819491fa6d2ebc6
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2022-02-15 23:31:54 +0200

    rounded buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'rounded_button.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -30,8 +32,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -43,23 +43,14 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -102,6 +93,16 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            RoundedButton(
+              color: Colors.blueAccent,
+              title: 'Up',
+              onPressed: () async {},
+            ),
+            RoundedButton(
+              color: Colors.blueAccent,
+              title: 'Down',
+              onPressed: () async {},
+            ),
           ],
         ),
       ),
@@ -111,5 +112,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 }
